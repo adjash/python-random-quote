3b09b39c47fbd86d68a29c6b1a7e659afaf93a17
adam hardie <adam.hardie13@gmail.com>
2020-11-23 14:46:51 +0000

    tpb theme added to the random quotes, added rickyisms/mispronunciations

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,33 @@
 import random 
 
-def primary():
-  print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
+#Prints random ricky misspeak
+def randomMispronunciations():
+  #read misspeak file, store lines
+  f = open("misspeak.txt")
   quotes = f.readlines()
   f.close()
+  #get last isms from list, generate rnd, find rnd between 0 and rnd
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  randomRickyism()
   print(quotes[rnd])
+  return quotes[rnd]
+
+#Prints a random rickyism
+def randomRickyism():
+  #Read rickyisms file, store lines
+  f = open("rickyisms.txt")
+  isms = f.readlines()
+  f.close()
+  #get last isms from list, generate rnd, find rnd between 0 and rnd
+  last = len(isms) - 1
+  rnd = random.randint(0, last)
+  print(isms[rnd])
+  return isms[rnd]
+
+
+
+
 
 if __name__== "__main__":
-  primary()
+  randomMispronunciations()
